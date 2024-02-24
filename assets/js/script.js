9447876f4b2401f19a3c6eff61936814cab35f43
Developer-Zahid <coderexpertzahid@gmail.com>
2024-02-24 19:27:27 +0600

    Baseflow-Slider-Clone init

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let swiper = new Swiper(".tinyflow-slider", {
+    slidesPerView: 5,
+    spaceBetween: 20,
+    centeredSlides: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
